63107b0966d113e4d217407609af80c49ad1e939
William Creighton <william.creighton@gmail.com>
2014-05-28 08:43:35 -0400

    Initial Commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,48 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+// $(function(){
+//     $('.fadein img:gt(0)').hide();
+//     setInterval(function(){
+//       $('.fadein :first-child').fadeOut()
+//          .next('img').fadeIn()
+//          .end().appendTo('.fadein');}, 
+//       3000);
+// });
+
+var imageArray = [
+	'u', 
+	'/images/five-stages-inebriation-01.jpg', 
+	'/images/five-stages-inebriation-02.jpg', 
+	'/images/five-stages-inebriation-03.jpg', 
+	'/images/five-stages-inebriation-04.jpg', 
+	'/images/five-stages-inebriation-05.jpg'
+]
+
+var imageArray = list[
+	'u',
+	'/images/five-stages-inebriation-01.jpg', 
+	'/images/five-stages-inebriation-02.jpg', 
+	'/images/five-stages-inebriation-03.jpg', 
+	'/images/five-stages-inebriation-04.jpg', 
+	'/images/five-stages-inebriation-05.jpg'
+]
+
+var imageArray = [
+	'/images/five-stages-inebriation-01.jpg', 
+	'/images/five-stages-inebriation-02.jpg', 
+	'/images/five-stages-inebriation-03.jpg', 
+	'/images/five-stages-inebriation-04.jpg', 
+	'/images/five-stages-inebriation-05.jpg'
+]
+
+
+
+
+function list(type) {
+  var result = "<" + type + "l><li>";
+  var args = Array.prototype.slice.call(arguments, 1);
+  result += args.join("</li><li>");
+  result += "</li></" + type + "l>"; // end list
+
+  return result;
+}
